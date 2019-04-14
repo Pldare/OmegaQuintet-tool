fd52d5764aa408535a19046a78cdf726f8253b27
Pldare <35677924+Pldare@users.noreply.github.com>
2019-04-14 20:49:46 +0800

    Little-Endian and Big-Endian Judge add
    Megadimension Neptunia VIIR cl3 and OmegaQuintet cl3 export is ok

--- OmegaQuintet_cl3L.rb
+++ OmegaQuintet_cl3L.rb
@@ -1,3 +1,19 @@
+require 'find'
+require 'pathname'
+lujin=Pathname.new(File.dirname(__FILE__)).realpath 
+file_name=[0]
+all_files=0
+Find.find(lujin) do |path|
+  #puts path
+  #if File::directory?(path)
+  tmp_nn=path.split("/")
+  org_name=tmp_nn[((tmp_nn.size.to_i)-1)].to_s
+  if /.cl3$/ =~ org_name
+	file_name[all_files]=org_name
+	all_files+=1
+  end
+end
+
 def str_magic(_str)
 	re_size=_str.to_s.size.to_i
 	r_wz=$cl3_file.read(re_size).to_s
@@ -21,49 +37,67 @@ def readstr
 	return _tmp_o.chomp("\x00")
 end
 def read_int32
-	return $cl3_file.read(4).unpack("V").to_s.gsub("[","").gsub("]","").to_i
+	return $cl3_file.read(4).unpack($endinn).to_s.gsub("[","").gsub("]","").to_i
 end
 def go_wz(_wz)
 	$cl3_file.seek(_wz)
 end
-def write_file(_file_name,_wz,_size)
-	_tmp_file=File.open(_file_name,"wb")
+def write_file(_floder,_file_name,_wz,_size)
+	#creat floder
+	mk_floder="md "+_floder.to_s
+	if File::directory?(_floder)
+	else
+		system mk_floder
+	end
+	
+	_tmp_file=File.open(((_floder.to_s)+"\\"+(_file_name.to_s)),"wb+")
 	go_wz(_wz)
 	_tmp_data=$cl3_file.read(_size)
 	_tmp_file.print _tmp_data
 	_tmp_file.close
 	puts "save #{_file_name}"
 end
-$cl3_file=File.open(ARGV[0],"rb")
-str_magic("CL3L")
-go_wz(16)
-wz=read_int32
-go_wz(wz)
-str_magic("FILE_COLLECTION")
-go_wz(wz+32)
-file_es,dunny,start_wz=read_int32,read_int32,read_int32
-wz_local=start_wz
 
-for i in 0..(file_es-1)
-	go_wz(wz_local)
-	file_name=readstr
-	wz_local+=512
-	go_wz(wz_local)
-	index=read_int32
-	file_local_wz=read_int32
-	file_size=read_int32
-	offsett=start_wz+file_local_wz
-	
-	#file-head
-	go_wz(offsett)
-	litte_head=$cl3_file.read(3).to_s
-	if litte_head == "DDS"
-		tmp_file_name=file_name.split(".")
-		tmp_file_name[((tmp_file_name.size.to_i)-1)]="dds"
-		file_name=tmp_file_name.join(".")
+for ii in 0..(all_files-1) 
+	puts "open #{file_name[ii]}"
+	$cl3_file=File.open(file_name[ii],"rb")
+	floder_name=file_name[ii].to_s.split(".")[0]
+	str_magic("CL3")
+	type=$cl3_file.read(1).to_s
+	if type == "L"
+		$endinn="V"
+	elsif type == "B"
+		$endinn="N"
 	end
-	
-	write_file(file_name,offsett,file_size)
-	wz_local+=48
+	go_wz(16)
+	wz=read_int32
+	go_wz(wz)
+	str_magic("FILE_COLLECTION")
+	go_wz(wz+32)
+	file_es,dunny,start_wz=read_int32,read_int32,read_int32
+	wz_local=start_wz
+
+	for i in 0..(file_es-1)
+		go_wz(wz_local)
+		n_file_name=readstr
+		wz_local+=512
+		go_wz(wz_local)
+		index=read_int32
+		file_local_wz=read_int32
+		file_size=read_int32
+		offsett=start_wz+file_local_wz
+		
+		#file-head
+		go_wz(offsett)
+		litte_head=$cl3_file.read(3).to_s
+		if litte_head == "DDS"
+			tmp_file_name=n_file_name.split(".")
+			tmp_file_name[((tmp_file_name.size.to_i)-1)]="dds"
+			n_file_name=tmp_file_name.join(".")
+		end
+		
+		write_file(floder_name,n_file_name,offsett,file_size)
+		wz_local+=48
+	end
+	$cl3_file.close
 end
-$cl3_file.close
